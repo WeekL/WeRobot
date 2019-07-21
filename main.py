5dbd39bb78b7dc69cbef6361b50237be072f5278
byron <lllx9464@qq.com>
2019-07-21 17:40:25 +0800

    fix path separator error in linux

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def text_reply(msg):
             else:
                 reply = u"{}".format(auto_text.get_content(receive))
     elif msg_type == 'Picture':
-        pic_path = fu.get_root_path() + 'data\\image\\'  # 图片保存路径
+        pic_path = fu.get_root_path() + "data%simage%s" % fu.get_path_separator() # 图片保存路径
         msg['Text'](fu.comfirm_dir(pic_path) + msg['FileName'])
         msg_content = pic_path + msg['FileName']
         if tag:
@@ -54,19 +54,19 @@ def text_reply(msg):
             else:
                 reply = auto_pic.get_content(pic_path + msg['FileName'])
     elif msg_type == 'Recording':
-        audio_path = fu.get_root_path() + 'data\\audio\\'  # 语音保存路径
+        audio_path = fu.get_root_path() + "data%saudio%s" % fu.get_path_separator()  # 语音保存路径
         msg['Text'](fu.comfirm_dir(audio_path) + msg['FileName'])
         msg_content = audio_path + msg['FileName']
         if tag:
             reply = auto_audio.get_reply(au.mp32wav(audio_path, msg['FileName']))
     elif msg_type == 'Video':
-        video_path = fu.get_root_path() + 'data\\video\\'
+        video_path = fu.get_root_path() + "data%svideo%s" % fu.get_path_separator()
         msg['Text'](fu.comfirm_dir(video_path) + msg['FileName'])
         msg_content = video_path + msg['FileName']
         if tag:
             reply = '啥？视频？'
     elif msg_type == 'Attachment':
-        attach_path = fu.get_root_path() + 'data\\attach\\'
+        attach_path = fu.get_root_path() + "data%sattch%s" % fu.get_path_separator()
         msg['Text'](fu.comfirm_dir(attach_path) + msg['FileName'])
         msg_content = attach_path + msg['FileName']
         if tag:
@@ -146,9 +146,11 @@ def anti_withdrawal(msg):
 
 if __name__ == '__main__':
     # itchat.auto_login(hotReload=True, statusStorageDir='newInstance.pkl')
-    itchat.auto_login(hotReload=False, enableCmdQR=2)
-    # itchat.auto_login(hotReload=True)
+    if fu.is_linux():
+        itchat.auto_login(hotReload=True, enableCmdQR=2)
+    else:
+        itchat.auto_login(hotReload=True)
 
     # 获取自己的UserName
     myUserName = itchat.get_friends(update=True)[0]["UserName"]
-    itchat.run(debug=True)
+    itchat.run(debug=False)
--- utils/file_util.py
+++ utils/file_util.py
@@ -1,9 +1,24 @@
 import os
+import platform
 
+def is_linux():
+    sys_str=platform.system()
+    if sys_str == 'Windows':
+        return False
+    else:
+        return True
+
+def get_path_separator():
+    if is_linux():
+        return '/'
+    else:
+        return '\\'
 
 def get_root_path():
     curPath = os.path.abspath(os.path.dirname(__file__))
     rootPath = curPath[:curPath.find("WeRobot\\") + len("WeRobot\\")]  # 获取WeRobot根路径
+    if is_linux():
+        rootPath = curPath[:curPath.find("WeRobot/") + len("WeRobot/")]
     return rootPath
 
 
